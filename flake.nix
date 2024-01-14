f8d76bb963e833546473d901f9c5716805c7ed4a
Theodor-Alexandru Irimia <Theodor.Irimia@schuettflix.com>
2024-01-14 13:55:54 +0100

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -3,38 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  /*
-  * Initial plan: hijack the bootstrapping process of GO to build my own version
-  * learned how self-hosted languages work from watching enough tsoding
-  * ******************
-  * Second idea: use tinygo's internal libraries to build the binary
-  * ******************
-  * Third iteration: use nix to build tinygo, embed it in serverfi,
-  * write it out to a file on run, exec it, clean up
-  * Turns out some essential libraries don't work with tinygo
-  * ******************
-  * Fourth iteration: use nix to embed go itself in serverfi
-  */
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [];
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        lib,
-        ...
-      }: {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
         formatter = pkgs.alejandra;
-        packages.default = let
-          go-compiler = pkgs.go_1_21;
-        in
-          pkgs.buildGoModule rec {
+        packages = {
+          default = self.outputs.packages.${system}.serverfi;
+          serverfi = pkgs.buildGoModule rec {
             pname = "serverfi";
             version = "0.1.0";
 
@@ -45,13 +32,14 @@
             CGO_ENABLED = 0;
 
             buildPhase = ''
-              cp -r ${go-compiler}/share/go .
+              cp -r ${pkgs.go}/share/go .
               ${pkgs.zip}/bin/zip -r go.zip . -i "./go/*"
               cp go.zip static/
               mkdir -p $out/bin
               go build -o $out/bin/serverfi main.go
             '';
           };
-      };
-    };
+        };
+      }
+    );
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module serverfi
 
-go 1.20
+go 1.21
